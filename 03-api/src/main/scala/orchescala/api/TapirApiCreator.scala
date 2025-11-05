aa1b5ccade00caa5b5737b574a74a6df89493680
Pascal Mengelt <pascal.mengelt@gmail.com>
2025-11-05 08:51:38 +0100

    Adjusted paths for gateway.

--- 03-api/src/main/scala/orchescala/api/TapirApiCreator.scala
+++ 03-api/src/main/scala/orchescala/api/TapirApiCreator.scala
@@ -2,6 +2,7 @@ package orchescala
 package api
 
 import orchescala.domain.*
+import orchescala.domain.InOutType.Bpmn
 import sttp.tapir.EndpointIO.Example
 
 trait TapirApiCreator extends AbstractApiCreator:
@@ -10,7 +11,7 @@ trait TapirApiCreator extends AbstractApiCreator:
     println(s"Start API: ${apiDoc.apis.size} top level APIs")
     apiDoc.apis.flatMap {
       case groupedApi: GroupedApi => groupedApi.create()
-      case cApi: CApi => throw IllegalArgumentException(
+      case cApi: CApi             => throw IllegalArgumentException(
           s"Sorry, the top level must be a GroupedApi (Group or Process)!\n - Not ${cApi.getClass}"
         )
     }
@@ -24,7 +25,7 @@ trait TapirApiCreator extends AbstractApiCreator:
           pa.createEndpoint(pa.id, false, pa.additionalDescr) ++
             pa.createInitEndpoint(pa.id) ++
             pa.apis.flatMap(_.create(pa.id, false))
-        case _: CApiGroup =>
+        case _: CApiGroup            =>
           groupedApi.apis.flatMap(_.create(groupedApi.name, true))
       end match
     end create
@@ -36,14 +37,14 @@ trait TapirApiCreator extends AbstractApiCreator:
       cApi match
         case da @ DecisionDmnApi(_, _, _, _) =>
           da.createEndpoint(tag, tagIsFix, da.additionalDescr)
-        case aa @ ActivityApi(_, _, _) =>
+        case aa @ ActivityApi(_, _, _)       =>
           aa.createEndpoint(tag, tagIsFix)
         case pa @ ProcessApi(name, _, _, apis, _)
             if apis.isEmpty =>
           pa.createEndpoint(tag, tagIsFix, pa.additionalDescr)
-        case spa: ExternalTaskApi[?, ?] =>
+        case spa: ExternalTaskApi[?, ?]      =>
           spa.createEndpoint(tag, tagIsFix, spa.additionalDescr)
-        case ga =>
+        case ga                              =>
           throw IllegalArgumentException(
             s"Sorry, only one level of GroupedApi is allowed!\n - $ga"
           )
@@ -51,6 +52,7 @@ trait TapirApiCreator extends AbstractApiCreator:
   end extension
 
   extension (processApi: ProcessApi[?, ?, ?])
+
     // creates the Init Worker Endpoint - each Process has one (not for GenericService)
     def createInitEndpoint(tagFull: String): Seq[PublicEndpoint[?, Unit, ?, Any]] =
       if hasInitIn then
@@ -59,10 +61,12 @@ trait TapirApiCreator extends AbstractApiCreator:
           endpoint
             .name("Init Worker")
             .tag(eTag)
-            .in(processApi.path(eTag) / "init")
+            .in("worker" / processApi.id)
             .summary("Init Worker")
             .description(
               s"""|${processApi.inOut.initInDescr.mkString}
+                  |<details>
+                  |<summary><b>Init Worker Details</b></summary>
                   |
                   |The Init Worker has the following responsibilities:
                   |
@@ -71,23 +75,26 @@ trait TapirApiCreator extends AbstractApiCreator:
                   |  - Custom validation the Process Input (`In`, e.g. combining 2 variables). -> Process Specific
                   |  - Initializes the default Variables with `In` as input (see _Process_ description) and `InitIn` as output. -> Process Specific
                   |
+                  |The Input is the same as the `In` object of the _Process_.
+                  |
                   |`NoInput` means there is no initialization needed.
+                  |</details>
                   |""".stripMargin
             )
-            .head
-        )// renders `In` as input: .map(ep => processApi.toInput.map(ep.in).getOrElse(ep))
-          .map(ep => ep.out(processApi.toInitIn))
+            .out(processApi.toInitIn)
+            .post
+        ) // renders `In` as input:
+          .map(ep => processApi.toInput.map(ep.in).getOrElse(ep))
       else
         Seq.empty
-    end createInitEndpoint
 
     private def hasInitIn: Boolean =
       processApi.inOut.initIn match
-        case _: NoOutput =>
+        case _: NoOutput                                     =>
           false
         case i if i.getClass == processApi.inOut.in.getClass =>
           false
-        case _ =>
+        case _                                               =>
           true
     end hasInitIn
 
@@ -108,55 +115,53 @@ trait TapirApiCreator extends AbstractApiCreator:
     def createEndpoint(
         tagFull: String,
         tagIsFix: Boolean,
-        additionalDescr: Option[String] = None
+        additionalDescr: Option[String] = None,
     ): Seq[PublicEndpoint[?, Unit, ?, Any]] =
-      val eTag = if tagIsFix then tagFull else shortenTag(tagFull)
+      val eTag         = if tagIsFix then tagFull else shortenTag(tagFull)
       println(s"createEndpoint: $tagIsFix $tagFull >> $eTag")
       val endpointName = if inOutApi.name == tagFull then "Process" else inOutApi.endpointName
+      println(s"Endpoint: $endpointName")
       Seq(
         endpoint
           .name(endpointName)
           .tag(eTag)
-          .in(path(eTag))
+          .in(endpointPath)
           .summary(endpointName)
           .description(
             inOutApi.apiDescription(apiConfig.companyName) +
               additionalDescr.getOrElse("")
           )
-          .head
+          .post
       ).map(ep => inOutApi.toInput.map(ep.in).getOrElse(ep))
         .map(ep => inOutApi.toOutput.map(ep.out).getOrElse(ep))
     end createEndpoint
 
-    def path(tag: String): EndpointInput[Unit] =
+    def endpointPath =
       val refId = refIdentShort(inOutApi.id, projectName)
-      val tagPath = tag.replace(" ", "")
-
-      inOutApi.inOut.in match
-        case gs: GenericServiceIn =>
-          inOutApi.inOutType.toString / refId / gs.serviceName
-        case _ if tagPath == refId =>
-          if inOutApi.name == refId then
-            inOutApi.inOutType.toString / refId
-          else
-            inOutApi.inOutType.toString / refId / inOutApi.name
-              .replace(" ", "")
-        case _ =>
-          if inOutApi.name == refId then
-            inOutApi.inOutType.toString / tagPath / refId
-          else
-            inOutApi.inOutType.toString / tagPath / refId / inOutApi.name.replace(
-              " ",
-              ""
-            )
+      val id    = inOutApi.id
+      inOutApi.inOutType match
+        case InOutType.Bpmn     =>
+          "process" / id / "async"
+        case InOutType.Worker   =>
+          "worker" / id
+        case InOutType.UserTask =>
+          "process" / path[String]("processInstanceId") / "userTask" / id / path[String]("userTaskInstanceId") / "complete"
+        case InOutType.Signal   =>
+          "signal" / id
+        case InOutType.Message  =>
+          "message" / id
+        case InOutType.Timer    =>
+          "timer" / id / "NOT_IMPLEMENTED"
+        case InOutType.Dmn      =>
+          "dmn" / id / "NOT_IMPLEMENTED"
       end match
-    end path
+    end endpointPath
 
     private def toInput: Option[EndpointInput[?]] =
       inOutApi.inOut.in match
         case _: NoInput =>
           None
-        case _ =>
+        case _          =>
           Some(
             inOutApi.inMapper
               .examples(inOutApi.apiExamples.inputExamples.fetchExamples.map {
@@ -173,7 +178,7 @@ trait TapirApiCreator extends AbstractApiCreator:
       inOutApi.inOut.out match
         case _: NoOutput =>
           None
-        case _ =>
+        case _           =>
           Some(
             inOutApi.outMapper
               .examples(inOutApi.apiExamples.outputExamples.fetchExamples.map {
@@ -193,12 +198,12 @@ trait TapirApiCreator extends AbstractApiCreator:
       pa.inOut.in match
         case gs: GenericServiceIn =>
           gs.serviceName
-        case _ => pa.id
+        case _                    => pa.id
 
     def additionalDescr: Option[String] =
       if apiConfig.projectsConfig.isConfigured then
         val usedByDescr = UsedByReferenceCreator(processName).create()
-        val usesDescr = UsesReferenceCreator(processName).create()
+        val usesDescr   = UsesReferenceCreator(processName).create()
         Some(s"\n\n${usedByDescr.mkString}${usesDescr.mkString}")
       else None
   end extension
