3093ecfbb37f919ea710d91bd6c2ff76be0198f2
Pascal Mengelt <pascal.mengelt@gmail.com>
2026-05-27 07:56:44 +0200

    Fixes not recovering from failure, in claiming tasks ind C7WorkerClient.

--- 02-engine/src/main/scala/orchescala/engine/rest/OAuth2Flow.scala
+++ 02-engine/src/main/scala/orchescala/engine/rest/OAuth2Flow.scala
@@ -3,13 +3,17 @@ package orchescala.engine.rest
 import sttp.client3.*
 import sttp.model.Uri
 
+import scala.concurrent.duration.*
+
 trait OAuth2Flow :
   protected def identityUrl: Uri
-  
+
   protected lazy val tokenRequest =
     basicRequest
       .post(identityUrl)
       .header("accept", "application/json")
       .header("content-type", "application/x-www-form-urlencoded")
+      .readTimeout(10.seconds)  // prevent indefinite blocking when SSO is unreachable
 
-  protected lazy val syncBackend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
+  protected lazy val syncBackend: SttpBackend[Identity, Any] =
+    HttpClientSyncBackend(options = SttpBackendOptions.connectionTimeout(10.seconds))
